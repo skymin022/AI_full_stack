c702bc93e41d0a57de857e68fd45ffadccee4ba6
skymin022 <wlqtk44@gmail.com>
2025-03-11 21:27:13 +0900

    test3

--- java/src/복습Day01/test.java
+++ java/src/복습Day01/test.java
@@ -1,7 +1,11 @@
 package 복습Day01;
 
 public class test {
-    System.out.println("test2");
+    public static void main(String[] args) {
+        System.out.println("test at home");
+        
+
+    }
 
     
 }
