434e2e68237c54e08c816b001249ca1925bd3402
tj-bu-706-03 <tj-bu-706-03@DESKTOP-U56C512>
2025-03-14 18:23:48 +0900

    2025_03_14(5일차)

---
+++ java/src/Day05/Ex09_Final/SuperClass.java
@@ -0,0 +1,20 @@
+package Day05.Ex09_Final;
+
+public class SuperClass {
+
+	// final 메소드 작성
+	public final void finalMethod() {
+		System.out.println("final 메소드 입니다.");
+	}
+	
+}
+
+
+class SubClass extends SuperClass {
+	// * final 메소드는 오버라이딩할 수 없다.
+	
+	
+	
+}
+
+	
